ac74ca0dada5d24a9580b213966dc8883118b6b5
hyunwoo <hw2621@daum.net>
2021-08-02 17:15:23 +0900

    트리...

--- BOJ/1289_트리의가중치.py
+++ BOJ/1289_트리의가중치.py
@@ -1,40 +1,27 @@
-import sys
-INF = sys.maxsize
-sys.setrecursionlimit(1000001)
-
-def weight(i,j):
-    # print(i,j)
-    if i == j:
-        return 1
-    if D[i].get(j,-1) != -1:
-        return D[i][j]
-    visited[i] = True
-    D[i][j] = INF
-    D[j][i] = INF
-    for nxt in range(1,N+1):
-        if nxt == i or edges[i].get(nxt,-1) == -1 or visited[nxt]:
+def weight(root):
+    visited[root] = True
+    child_weights = []
+    # 만약 자식 없으면 1 리턴
+    for dist,next in adj[root]:
+        if visited[next]:
             continue
-        temp = edges[i][nxt] * weight(nxt,j) % 1000000007
-        D[i][j] = min(D[i][j],temp)
-        D[j][i] = D[i][j]
-    visited[i] = False
-    return D[i][j]
+        child_weights.append(dist*weight(next))
+    # 있으면 배열 1개에 다 넣어놈
+    if child_weights == []:
+        return 1
+    else:
+        # 배열에 넣은애들 다 더해서 리턴
+        ret = 0
+        for i in range(len(child_weights)):
+            for j in range(i+1,len(child_weights)):
+                ret += child_weights[i][j]
+
 
 N = int(input())
-edges = [dict() for i in range(N+1)]
-D = [dict() for i in range(N+1)]
+adj = [[] for _ in range(N+1)]
 for _ in range(N-1):
     A,B,W = map(int,input().split())
-    edges[A][B] = W
-    edges[B][A] = W
-    D[A][B] = W
-    D[B][A] = W
-
-
-ans = 0
-for i in range(1,N+1):
-    for j in range(i+1,N+1):
-        visited = [False for _ in range(N+1)]
-        ans += weight(i,j) % 1000000007
-
-print(ans)
+    adj[A].append([W,B])
+    adj[B].append([W,A])
+visited = [False for _ in range(N+1)]
+print(weight(1))
